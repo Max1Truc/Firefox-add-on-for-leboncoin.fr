4b8c037321df660cfa8509cf848b027d79a40d26
Max1Truc <26603929+Max1Truc@users.noreply.github.com>
2018-03-21 16:24:59 +0100

    First fonctionnal commit! You can now copy LBC's ads.

--- Source/loadInForm.js
+++ Source/loadInForm.js
@@ -1,41 +1,20 @@
 // @name         Leboncoin (loader)
-// @description  Userscript pour pomper des annonces Leboncoin
+// @description  Userscript pour charger une annonces Leboncoin
 // @author       Max1Truc
 
-function load () {
-	function execInCurrentTab (theFunction) {
-	  browser.tabs.executeScript({
-	    code: '(' + theFunction + ')();'
-	  }, (results) => {
-	    // No log
-	  });
-	}
+setInterval(async () => {
+  var object = localStorage.getItem('save');
+  if (object != null) {
+    object = JSON.parse(object);
+    var title = object.title,
+      description = object.description,
+      price = object.price.replace(" \u20AC", "");
 
-	execInCurrentTab (() => {
-	    function getAllElementsWithAttributeValue(attribute, value)
-	    {
-	        var matchingElements = [];
-	        var allElements = document.getElementsByTagName('*');
-	        for (var i = 0, n = allElements.length; i < n; i++)
-	        {
-	            if (allElements[i].getAttribute(attribute) == value)
-	            {
-	                matchingElements.push(allElements[i]);
-	            }
-	        }
-	        return matchingElements;
-	    }
+    document.getElementById("subject").value = title;
+    document.getElementById("body").value = description;
+    document.getElementById("price").value = price;
 
-	    var object = JSON.parse(localStorage.getItem("save")),
-	        title = object.title,
-	        description = object.description,
-	        price = object.price.replace(" \u20AC", "");
-
-	    document.getElementById("subject").value = title;
-	    document.getElementById("body").value = description;
-	    document.getElementById("price").value = price;
-			document.getElementById("rs").click()
-
-	    alert("Annonce restauree !");
-	});
-}
+    // Resets the save
+    localStorage.removeItem("save");
+  }
+}, 500);
--- Source/saveFromAd.js
+++ Source/saveFromAd.js
@@ -1,12 +1,12 @@
-// @name         Leboncoin (pompe)
-// @description  Userscript pour pomper des annonces Leboncoin
+// @name         Leboncoin (saver)
+// @description  Userscript pour enregistrer une annonce Leboncoin
 // @author       Max1Truc
 
-function setup() {
-  getAllElementsWithThisInnerTextValue = (value) => {
-    all = document.getElementsByTagName("*");
-    list = [];
-    for (x in all) {
+function modifyDOM() {
+  function getAllElementsWithThisInnerTextValue(value) {
+    var all = document.getElementsByTagName("*");
+    var list = [];
+    for (let x in all) {
       if (all[x].innerText == value) {
         list.push(all[x]);
       }
@@ -15,41 +15,29 @@ function setup() {
     return list;
   }
 
-  modifyButton = getAllEWithThisInnerTextValue("Modifier l'annonce")[2];
-  modifyButton.setAttribute("class", "");
-  modifyButton.style.color = "green";
-  modifyButton.setEventListener("click", save)
-
-  function save() {
-    function execInCurrentTab(theFunction) {
-      browser.tabs.executeScript({
-        code: '(' + theFunction + ')();'
-      }, (results) => {
-        // No log
-      });
-    }
-
-    execInCurrentTab(() => {
-      function getAllElementsWithAttributeValue(attribute, value) {
-        var matchingElements = [];
-        var allElements = document.getElementsByTagName('*');
-        for (var i = 0, n = allElements.length; i < n; i++) {
-          if (allElements[i].getAttribute(attribute) == value) {
-            matchingElements.push(allElements[i]);
-          }
-        }
-        return matchingElements;
+  function getAllElementsWithAttributeValue(attribute, value) {
+    var matchingElements = [];
+    var allElements = document.getElementsByTagName('*');
+    for (var i = 0, n = allElements.length; i < n; i++) {
+      if (allElements[i].getAttribute(attribute) == value) {
+        matchingElements.push(allElements[i]);
       }
-
-      var title = document.getElementsByClassName("_1KQme")[0].innerText,
-        description = getAllElementsWithAttributeValue("data-qa-id", "adview_description_container")[0].children[0].innerText,
-        price = document.getElementsByClassName("_1F5u3")[0].innerText;
-
-      localStorage.setItem("save", JSON.stringify({
-        title: title,
-        description: description,
-        price: price
-      }));
-    });
+    }
+    return matchingElements;
   }
+
+  var modifyButton = getAllElementsWithThisInnerTextValue("Modifier l'annonce")[0];
+  var modifyButtonText = getAllElementsWithThisInnerTextValue("Modifier l'annonce")[2];
+  modifyButtonText.setAttribute("class", "");
+  modifyButtonText.style.color = "green";
+  modifyButton.removeAttribute("href");
+
+  modifyButton.addEventListener("click", () => {
+    var title = document.getElementsByClassName("_1KQme")[0].innerText,
+      description = getAllElementsWithAttributeValue("data-qa-id", "adview_description_container")[0].children[0].innerText,
+      price = document.getElementsByClassName("_1F5u3")[0].innerText;
+    localStorage.setItem("save", JSON.stringify({title, description, price}));
+  });
 }
+
+setInterval(modifyDOM, 500);
