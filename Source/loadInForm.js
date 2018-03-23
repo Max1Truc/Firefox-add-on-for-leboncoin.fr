af495f3c67ed1fcf6e2251ffd173de18d341d521
Max1Truc <maxitruc.bonbec@gmail.com>
2018-03-23 19:30:01 +0100

    Fixed some minor bugs

--- Source/loadInForm.js
+++ Source/loadInForm.js
@@ -4,15 +4,19 @@
 
 setInterval(async () => {
   var object = localStorage.getItem('save');
-  if (object != null) {
+  var title_input = document.getElementById("subject"),
+    description_input = document.getElementById("body"),
+    price_input = document.getElementById("price");
+
+  if (object != null && title_input.value == "" && description_input.value == "" && price_input.value == "") {
     object = JSON.parse(object);
     var title = object.title,
       description = object.description,
       price = object.price.replace(" \u20AC", "");
 
-    document.getElementById("subject").value = title;
-    document.getElementById("body").value = description;
-    document.getElementById("price").value = price;
+    title_input.value = title;
+    description_input.value = description;
+    price_input.value = price;
 
     // Resets the save
     localStorage.removeItem("save");
