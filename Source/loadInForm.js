06ebbe8b85c83e47314e7f0655fd3a4851920532
Max1Truc <maxitruc.bonbec@gmail.com>
2019-06-16 17:07:21 +0200

    Ajout de la copie des images ! L'extension copie maintenant les images en bonus.

--- Source/loadInForm.js
+++ Source/loadInForm.js
@@ -2,7 +2,7 @@
 // @description  Userscript pour charger une annonces Leboncoin
 // @author       Max1Truc
 
-setInterval(async () => {
+var intervalID = setInterval(() => {
   function selectCategory(category) {
     Array.from(document.getElementById("category").options).forEach((option) => {
       if (option.innerText == category) {
@@ -17,17 +17,52 @@ setInterval(async () => {
     description_input = document.getElementById("body"),
     price_input = document.getElementById("price");
 
+  if (title_input != undefined && category_input != undefined && description_input != undefined && price_input != undefined)
+    // Page fully loaded
+    clearInterval(intervalID);
+
   if (object != null && title_input.value == "" && description_input.value == "" && price_input.value == "") {
     object = JSON.parse(object);
     var title = object.title,
       category = object.category,
       description = object.description,
-      price = object.price.replace(" \u20AC", "");
+      price = object.price.replace(" \u20AC", ""); // Removes "€"
 
     title_input.value = title;
     selectCategory(category);
     description_input.value = description;
     price_input.value = price;
+
+    // Load the images
+    for (let image_id = 0; image_id < 3; image_id++) { // For each one of the three images
+      setTimeout(() => {
+        window.eval("(" + ((image_id) => { // Execute following code with window scope
+          function dataURLtoFile(dataurl, filename) {
+            var arr = dataurl.split(','),
+              mime = arr[0].match(/:(.*?);/)[1],
+              bstr = atob(arr[1]),
+              n = bstr.length,
+              u8arr = new Uint8Array(n);
+            while (n--) {
+              u8arr[n] = bstr.charCodeAt(n);
+            }
+            return new File([u8arr], filename, {
+              type: mime
+            });
+          }
+
+          var dataurl = localStorage.getItem("image" + image_id);
+          const dT = new DataTransfer();
+          var file = dataURLtoFile(dataurl, "image.jpg");
+
+          dT.items.add(file);
+          document.getElementById("image" + image_id).files = dT.files;
+          arrayOfFileUploadNewad[image_id].uploadStart("photo_upload_ajax", "verify", 300);
+
+          localStorage.removeItem("image" + image_id);
+        }).toString() + ")(" + image_id + ")");
+      }, (image_id) * 2500);
+    }
 
     // Resets the save
     localStorage.removeItem("save");
--- Source/saveFromAd.js
+++ Source/saveFromAd.js
@@ -26,27 +26,56 @@ var intervalId = setInterval(() => {
     return matchingElements;
   }
 
-  var modifyButton = getAllElementsWithThisInnerTextValue("Modifier l’annonce")[0];
-  var modifyButtonText = getAllElementsWithThisInnerTextValue("Modifier l’annonce")[2];
-  modifyButtonText.setAttribute("class", "");
-  modifyButtonText.style.color = "green";
-  modifyButton.removeAttribute("href");
-
-  modifyButton.addEventListener("click", () => {
+  function adDataBackup() {
+    // Save ad data
     var title = document.getElementsByTagName("h1")[0].innerText,
       category = getAllElementsWithAttributeValue("data-qa-id", "breadcrumb-item-2")[0].innerText,
       description = getAllElementsWithAttributeValue("data-qa-id", "adview_description_container")[0].children[0].innerText,
       price = document.getElementsByClassName("_1F5u3")[0].innerText;
+
     localStorage.setItem("save", JSON.stringify({
       title,
       category,
       description,
       price
     }));
+  }
 
+  function getDataURL(div, callback) {
+    var url = div.style.backgroundImage.split('("')[1].split('")')[0];
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+      var reader = new FileReader();
+      reader.onloadend = function() {
+        callback(reader.result);
+      }
+      reader.readAsDataURL(xhr.response);
+    };
+    xhr.open('GET', url);
+    xhr.responseType = 'blob';
+    xhr.send();
+  }
+
+  function photosBackup() {
+    // Backup the images
+    var all_images = document.querySelectorAll('div[alt=""]');
+    for (let i = 0; i < all_images.length; i++) {
+      getDataURL(all_images[i], (result) => localStorage.setItem("image" + i, result));
+    };
+  }
+
+  var modifyButton = getAllElementsWithThisInnerTextValue("Modifier l’annonce")[0];
+  modifyButton.outerHTML = "<a>" + modifyButton.innerHTML + "</a>"
+
+  var modifyButtonText = getAllElementsWithThisInnerTextValue("Modifier l’annonce")[2];
+  modifyButtonText.setAttribute("class", "");
+  modifyButtonText.style.color = "green";
+  modifyButtonText.addEventListener("click", () => {
+    window.setTimeout(adDataBackup, 50);
+    window.setTimeout(photosBackup, 50);
     window.open('https://www.leboncoin.fr/ai?ca=12_s'); // Opens a new tab to create a new ad
   });
 
-  // If this interval is finished, it disallow other intervals to repatch the page and prevents bugs.
+  // If this interval is finished, it disallows other iterations of this interval to repatch the page and prevents bugs.
   clearInterval(intervalId);
 }, 500);
